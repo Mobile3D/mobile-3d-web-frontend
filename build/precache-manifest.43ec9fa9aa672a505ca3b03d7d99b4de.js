d3d7bbd92c899c50d4886eafd801d22d481041c9
PoscherAlexander <40146720150381@litec.ac.at>
2020-01-09 18:33:34 +0100

    add build version

---
+++ build/precache-manifest.43ec9fa9aa672a505ca3b03d7d99b4de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f88cdcabf6817dc53fd020c305fe171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211ce573786422a2ceef",
+    "url": "/static/css/main.db455af5.chunk.css"
+  },
+  {
+    "revision": "e45cb867a590f4bf2d69",
+    "url": "/static/js/2.91c61197.chunk.js"
+  },
+  {
+    "revision": "211ce573786422a2ceef",
+    "url": "/static/js/main.3095e268.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5757a8ee142f2f324b01ad96fb12e521",
+    "url": "/static/media/icon-blue.5757a8ee.svg"
+  },
+  {
+    "revision": "3260c5c7722f0759df8c1e66b3c26ae3",
+    "url": "/static/media/icon-white.3260c5c7.svg"
+  }
+]);
